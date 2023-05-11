144d9f317c00edfabf496a6223053d698ea8ab7b
Vereshchaka Vitalii <vitaliy.vereshchaka@extern.a1.at>
2023-05-11 12:43:19 +0300

    pipeline

--- src/main/java/com/sytoss/csm/CSMApplication.java
+++ src/main/java/com/sytoss/csm/CSMApplication.java
@@ -1,13 +1,7 @@
 package com.sytoss.csm;
 
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import liquibase.sdk.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class CSMApplication {
