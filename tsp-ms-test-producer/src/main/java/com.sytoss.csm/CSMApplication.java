f1d9f444e8e3648738cc006457ba91c8953c66a3
Vereshchaka Vitalii <vitaliy.vereshchaka@extern.a1.at>
2023-05-11 14:04:14 +0300

    modules

---
+++ tsp-ms-test-producer/src/main/java/com.sytoss.csm/CSMApplication.java
@@ -0,0 +1,12 @@
+package com.sytoss.csm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CSMApplication.class, args);
+    }
+
+}
