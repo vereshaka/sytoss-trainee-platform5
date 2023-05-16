cf3f72cea363f026e14d6caa3177fb1c30efe7fb
Kornienko Dmytro <dmytro.kornienko@extern.a1.at>
2023-05-16 14:32:16 +0300

    [STP-55] fix enum

--- stp-ms-test-producer/src/main/java/bom/AnswerStatus.java
+++ stp-ms-test-producer/src/main/java/bom/AnswerStatus.java
@@ -1,20 +1,8 @@
 package bom;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public enum AnswerStatus {
-    STATUS_IN_PROGRESS(0),
-    STATUS_NOT_STARTED(1),
-    STATUS_NOT_ANSWERED(2),
-    STATUS_NOT_GRADED(3);
-
-
-    @Setter
-    @Getter
-    private Integer status;
-
-    AnswerStatus(Integer status) {
-        this.status = status;
-    }
+    STATUS_IN_PROGRESS,
+    STATUS_NOT_STARTED,
+    STATUS_NOT_ANSWERED,
+    STATUS_NOT_GRADED
 }
--- stp-ms-test-producer/src/main/java/bom/PersonalExamStatus.java
+++ stp-ms-test-producer/src/main/java/bom/PersonalExamStatus.java
@@ -1,18 +1,7 @@
 package bom;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public enum PersonalExamStatus {
-    STATUS_IN_PROGRESS(0),
-    STATUS_NOT_STARTED(1),
-    STATUS_FINISHED(2);
-
-    @Setter
-    @Getter
-    private Integer status;
-
-    PersonalExamStatus(Integer status) {
-        this.status = status;
-    }
+    IN_PROGRESS,
+    NOT_STARTED,
+    FINISHED
 }
