9752ed23a384fe8126ba1030aefe0eb8226aa278
Kornienko Dmytro <dmytro.kornienko@extern.a1.at>
2023-05-18 18:07:57 +0300

    [STP-39] fix comments

--- stp-ms-test-producer/src/test/java/com/sytoss/producer/controllers/PersonalExamControllerTest.java
+++ stp-ms-test-producer/src/test/java/com/sytoss/producer/controllers/PersonalExamControllerTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-public class PersonalControllerTest extends AbstractControllerTest {
+public class PersonalExamControllerTest extends AbstractControllerTest {
 
     @InjectMocks
     private PersonalExamController personalExamController;
