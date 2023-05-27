f943f706b89383db4562ef391327cfe395268c43
YehorBodenchuk <120697528+YehorBodenchuk@users.noreply.github.com>
2023-05-27 15:21:45 +0300

    [STP-63] Added BOM

---
+++ stp-domain/src/main/java/com/sytoss/domain/bom/lessons/Exam.java
@@ -0,0 +1,24 @@
+package com.sytoss.domain.bom.lessons;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+public class Exam {
+
+    private Long id;
+
+    private Date relevantFrom;
+
+    private Date relevantTo;
+
+    private Integer duration;
+
+    private Long groupId;
+
+    private List<Topic> topics;
+}
---
+++ stp-domain/src/main/java/com/sytoss/domain/bom/lessons/Group.java
@@ -0,0 +1,13 @@
+package com.sytoss.domain.bom.lessons;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Group {
+
+    private Long id;
+
+    private String name;
+}
