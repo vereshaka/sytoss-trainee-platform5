547aad954ae9ea33d1914e0d1a129740319714cc
Alexey Shatokhin <alexey.shatokhin@sytoss.com>
2023-05-29 13:06:59 +0200

    [STP-61] fix bugs

--- stp-ms-test-producer/src/test/java/com/sytoss/producer/AbstractApplicationTest.java
+++ stp-ms-test-producer/src/test/java/com/sytoss/producer/AbstractApplicationTest.java
@@ -53,22 +53,26 @@ public abstract class AbstractApplicationTest extends AbstractJunitTest {
         }
     }
 
-    protected <T> ResponseEntity<T> perform(String uri, HttpMethod method, Object requestEntity, Class<T> responseType) {
-        HttpHeaders headers = new HttpHeaders();
-        if (requestEntity instanceof HttpEntity && ((HttpEntity) requestEntity).getHeaders() != null) {
-            for (Map.Entry entry : ((HttpEntity) requestEntity).getHeaders().entrySet()) {
-                headers.addAll(entry.getKey().toString(), (List<? extends String>) entry.getValue());
-            }
-        }
-        HttpEntity request = new HttpEntity<>(requestEntity, headers);
-        return restTemplate.exchange(getEndpoint(uri), method, request, responseType);
+//    protected <T> ResponseEntity<T> perform(String uri, HttpMethod method, Object requestEntity, Class<T> responseType) {
+//        HttpHeaders headers = new HttpHeaders();
+//        if (requestEntity instanceof HttpEntity && ((HttpEntity) requestEntity).getHeaders() != null) {
+//            for (Map.Entry entry : ((HttpEntity) requestEntity).getHeaders().entrySet()) {
+//                headers.addAll(entry.getKey().toString(), (List<? extends String>) entry.getValue());
+//            }
+//        }
+//        HttpEntity request = new HttpEntity<>(requestEntity, headers);
+//        return restTemplate.exchange(getEndpoint(uri), method, request, responseType);
+//    }
+
+    protected <T> ResponseEntity<T> perform(String uri, HttpMethod method, HttpEntity requestEntity, Class<T> responseType) {
+        return restTemplate.exchange(getEndpoint(uri), method, requestEntity, responseType);
     }
 
-    public <T> ResponseEntity<T> doPost(String uri, Object requestEntity, Class<T> responseType) {
+    public <T> ResponseEntity<T> doPost(String uri, HttpEntity requestEntity, Class<T> responseType) {
         return perform(uri, HttpMethod.POST, requestEntity, responseType);
     }
 
-    public <T> ResponseEntity<T> doGet(String uri, Object requestEntity, Class<T> responseType) {
+    public <T> ResponseEntity<T> doGet(String uri, HttpEntity requestEntity, Class<T> responseType) {
         return perform(uri, HttpMethod.GET, requestEntity, responseType);
     }
 }
