71b730faf382ee9349d4b0d3d85366f67f9c3e61
Rasvel <ivan.larin@sytoss.com>
2023-05-29 21:53:53 +0300

    [STP-43] Added api responses

--- stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/controller/CheckTaskController.java
+++ stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/controller/CheckTaskController.java
@@ -3,6 +3,7 @@ package com.sytoss.checktask.stp.controller;
 import bom.CheckAnswerRequestBody;
 import com.sytoss.checktask.stp.service.GradeService;
 import com.sytoss.domain.bom.personalexam.Grade;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class CheckTaskController {
 
     private final GradeService gradeService;
 
+    @ApiResponse(responseCode = "200", description = "Success|OK")
     @RequestMapping(method = RequestMethod.POST)
     public Grade check(
             @RequestBody CheckAnswerRequestBody body) {
