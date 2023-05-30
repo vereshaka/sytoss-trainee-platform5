c4a9727322c0b372c5a00685b4b39704b7951d07
Maksym Mitkov <maxitan1701@gmail.com>
2023-05-30 11:03:38 +0300

    [STP-13] fix doPost

--- stp-ms-lessons/src/test/java/com/sytoss/lessons/bdd/when/DisciplineWhen.java
+++ stp-ms-lessons/src/test/java/com/sytoss/lessons/bdd/when/DisciplineWhen.java
@@ -1,25 +1,26 @@
 package com.sytoss.lessons.bdd.when;
 
 import com.sytoss.domain.bom.lessons.Discipline;
+import com.sytoss.domain.bom.users.Group;
 import com.sytoss.lessons.bdd.CucumberIntegrationTest;
 import com.sytoss.lessons.bdd.common.TestExecutionContext;
 import io.cucumber.java.en.When;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public class DisciplineWhen extends CucumberIntegrationTest {
 
     @When("teacher creates {string} discipline")
     public void topicCreating(String disciplineName) {
         String url = "/api/teacher/" + TestExecutionContext.getTestContext().getTeacherId() + "/discipline/create";
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         Discipline discipline = new Discipline();
         discipline.setName(disciplineName);
-        HttpEntity<Discipline> request = new HttpEntity<>(discipline, httpHeaders);
-        ResponseEntity<String> responseEntity = getRestTemplate().postForEntity(url, request, String.class);
+        ResponseEntity<String> responseEntity = doPost(url, discipline, new ParameterizedTypeReference<String>(){});
         TestExecutionContext.getTestContext().setResponse(responseEntity);
     }
 }
