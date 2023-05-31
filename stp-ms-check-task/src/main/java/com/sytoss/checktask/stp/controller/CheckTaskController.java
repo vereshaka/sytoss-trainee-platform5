672b39834661af86c9da094fc5a7a91964fbc890
Rasvel <ivan.larin@sytoss.com>
2023-05-31 09:50:19 +0300

    [STP-43] Moved exception handling to CheckTaskController.java

--- stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/controller/CheckTaskController.java
+++ stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/controller/CheckTaskController.java
@@ -3,7 +3,9 @@ package com.sytoss.checktask.stp.controller;
 import com.sytoss.checktask.model.CheckTaskParameters;
 import com.sytoss.checktask.stp.service.GradeService;
 import com.sytoss.domain.bom.personalexam.Grade;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,10 @@ public class CheckTaskController {
 
     private final GradeService gradeService;
 
-    @ApiResponse(responseCode = "200", description = "Success|OK")
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "Success|OK"), @ApiResponse(responseCode = "400",
+                    description = "Bad request", content = @Content(mediaType = "")), @ApiResponse(responseCode = "406",
+                    description = "Not Acceptable", content = @Content(mediaType = ""))})
     @RequestMapping(method = RequestMethod.POST)
     public Grade check(
             @RequestBody CheckTaskParameters body) {
--- stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/controller/GlobalControllerExceptionHandler.java
+++ stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/controller/GlobalControllerExceptionHandler.java
@@ -2,8 +2,6 @@ package com.sytoss.checktask.stp.controller;
 
 import com.sytoss.checktask.stp.exceptions.DatabaseCommunicationException;
 import com.sytoss.checktask.stp.exceptions.WrongEtalonException;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,15 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler({DatabaseCommunicationException.class})
-    @ApiResponse(responseCode = "400",
-            description = "Bad request", content = @Content(mediaType = ""))
     public ResponseEntity<?> handleValidationException(DatabaseCommunicationException databaseCommunicationException) {
         return ResponseEntity.status(400).body(databaseCommunicationException.getMessage());
     }
 
     @ExceptionHandler({WrongEtalonException.class})
-    @ApiResponse(responseCode = "406",
-            description = "Not Acceptable", content = @Content(mediaType = ""))
     public ResponseEntity<?> handleValidationException(WrongEtalonException wrongEtalonException) {
         return ResponseEntity.status(406).body(wrongEtalonException.getMessage());
     }
