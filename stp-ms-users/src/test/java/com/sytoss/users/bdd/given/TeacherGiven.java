00b6246a9f285de3d8dcf2eeab8d22722099e518
Vasylyev Yevgeniy <yevgeny.vasilyev@extern.a1.at>
2023-06-19 14:02:25 +0300

    [STP-142] Fix BDD test

--- stp-ms-users/src/test/java/com/sytoss/users/bdd/given/TeacherGiven.java
+++ stp-ms-users/src/test/java/com/sytoss/users/bdd/given/TeacherGiven.java
@@ -6,9 +6,9 @@ import io.cucumber.java.en.Given;
 
 public class TeacherGiven extends CucumberIntegrationTest {
 
-    @Given("^teacher with \"(.*)\" firstName and \"(.*)\" middleName and \"(.*)\" lastName doesnt exist$")
+    @Given("^teacher with \"(.*)\" firstName and \"(.*)\" lastName doesnt exist$")
     public void teacherNotExist(String firstName, String middleName, String lastName) {
-        UserDTO teacherDTO = getTeacherConnector().getByFirstNameAndMiddleNameAndLastName(firstName, middleName, lastName);
+        UserDTO teacherDTO = getTeacherConnector().getByFirstNameAndLastName(firstName, lastName);
         if (teacherDTO != null) {
             getTeacherConnector().delete(teacherDTO);
         }
--- stp-ms-users/src/test/resources/features/teacher.feature
+++ stp-ms-users/src/test/resources/features/teacher.feature
@@ -1,7 +1,7 @@
 Feature: Teacher
 
   Scenario: registration teacher
-    Given teacher with "Ivan" firstName and "Ivanov" middleName and "Ivanovich" lastName doesnt exist
+    Given teacher with "Ivan" firstName and "Ivanovich" lastName doesnt exist
     When anonymous register in system as teacher with "Ivan" firstname and "Ivanov" middlename and "Ivanovich" lastname
     Then operation is successful
     And teacher with "Ivan" firstname and "Ivanov" middlename and "Ivanovich" lastname should exist
