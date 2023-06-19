1b1cff6be6071381d69d2b5a24ea9941240d4659
Vasylyev Yevgeniy <yevgeny.vasilyev@extern.a1.at>
2023-06-19 13:48:52 +0300

    [STP-142] Renaming of variables

--- stp-ms-users/src/main/java/com/sytoss/users/dto/UserDTO.java
+++ stp-ms-users/src/main/java/com/sytoss/users/dto/UserDTO.java
@@ -13,8 +13,8 @@ public abstract class UserDTO {
 
     @Id
     @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "teacher_id_generator")
-    @SequenceGenerator(name = "teacher_id_generator", sequenceName = "APP_USER_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_generator")
+    @SequenceGenerator(name = "user_id_generator", sequenceName = "APP_USER_SEQ", allocationSize = 1)
     private Long id;
 
     @Column(name = "FIRST_NAME")
@@ -22,9 +22,6 @@ public abstract class UserDTO {
 
     @Column(name = "LAST_NAME")
     private String lastName;
-
-    @Column(name = "MIDDLE_NAME")
-    private String middleName;
 
     @Column(name = "EMAIL")
     private String email;
