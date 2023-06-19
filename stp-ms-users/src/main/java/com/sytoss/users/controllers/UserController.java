3487a66deee9ccfe06b49d92bf0118cec3f0b85b
Vasylyev Yevgeniy <yevgeny.vasilyev@extern.a1.at>
2023-06-19 15:39:09 +0300

    [STP-142] Refactor code

--- stp-ms-users/src/main/java/com/sytoss/users/controllers/UserController.java
+++ stp-ms-users/src/main/java/com/sytoss/users/controllers/UserController.java
@@ -2,12 +2,16 @@ package com.sytoss.users.controllers;
 
 import com.sytoss.domain.bom.users.AbstractUser;
 import com.sytoss.users.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/user")
@@ -22,5 +26,14 @@ public class UserController {
     public AbstractUser me() {
         String email = ((Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getClaim("email");
         return userService.getOrCreateUser(email);
+    }
+
+    @Operation(description = "Method that updates photo of student")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success|OK"),
+    })
+    @PostMapping("/updatePhoto")
+    public void updatePhoto(@RequestParam MultipartFile photo) {
+        userService.updatePhoto(photo);
     }
 }
