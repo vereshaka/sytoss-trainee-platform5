2238e61b1ea9f1f65ca87243038ded63410c3f4d
Rasvel <ivan.larin@sytoss.com>
2023-06-29 14:53:02 +0300

    [STP-183] - Added cucumber test

--- stp-ms-image-provider/src/test/java/com/sytoss/provider/bdd/CucumberIntegrationTest.java
+++ stp-ms-image-provider/src/test/java/com/sytoss/provider/bdd/CucumberIntegrationTest.java
@@ -2,11 +2,23 @@ package com.sytoss.provider.bdd;
 
 import com.sytoss.provider.connector.ImageConnector;
 import com.sytoss.stp.test.cucumber.StpIntegrationTest;
+import com.sytoss.stp.test.cucumber.TestExecutionContext;
+import io.cucumber.spring.CucumberContextConfiguration;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-
 @Getter
+@CucumberContextConfiguration
 public class CucumberIntegrationTest extends StpIntegrationTest {
+
+    @Override
+    protected TestExecutionContext<?> getTestExecutionContext() {
+        return super.getTestExecutionContext();
+    }
+
+    @Override
+    protected Object createDetails() {
+        return null;
+    }
 
     @Autowired
     private ImageConnector imageConnector;
