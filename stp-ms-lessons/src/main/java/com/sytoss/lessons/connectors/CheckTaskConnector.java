51ec377917e52182931da71c9b2cd80d9eaf06dc
Rasvel <ivan.larin@sytoss.com>
2023-07-20 22:09:14 +0300

    [STP-3++] - Fixed

--- stp-ms-lessons/src/main/java/com/sytoss/lessons/connectors/CheckTaskConnector.java
+++ stp-ms-lessons/src/main/java/com/sytoss/lessons/connectors/CheckTaskConnector.java
@@ -5,6 +5,7 @@ import com.sytoss.domain.bom.personalexam.CheckRequestParameters;
 import com.sytoss.domain.bom.personalexam.IsCheckEtalon;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(url = "${check-task-url}", name = "checkTaskConnector")
@@ -13,6 +14,6 @@ public interface CheckTaskConnector {
     @GetMapping("/task/check-etalon")
     IsCheckEtalon checkEtalon(@RequestBody CheckRequestParameters body);
 
-    @GetMapping("/task/check-request")
+    @PostMapping("/task/check-request")
     QueryResult checkRequest(@RequestBody CheckRequestParameters body);
 }
