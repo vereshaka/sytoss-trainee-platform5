a09b41b108fe6da4d1b18569819896e65a468dc6
yevgenyv <yevgeny.vasilyev@sytoss.com>
2023-07-20 21:08:36 +0300

    [STP-xxx] Fix url of feign client

--- stp-ms-lessons/src/main/java/com/sytoss/lessons/connectors/CheckTaskConnector.java
+++ stp-ms-lessons/src/main/java/com/sytoss/lessons/connectors/CheckTaskConnector.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(url = "http://localhost:9010/api", name = "checkTaskConnector")
+@FeignClient(url = "${check-task-url}", name = "checkTaskConnector")
 public interface CheckTaskConnector {
 
     @GetMapping("/task/check-etalon")
