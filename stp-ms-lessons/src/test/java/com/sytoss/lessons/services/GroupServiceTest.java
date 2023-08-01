65c5b1114b53ec0062e0a4eee03fedbe3eaf2cc3
Rasvel <ivan.larin@sytoss.com>
2023-08-01 18:05:33 +0300

    [STP-352] - Change return

--- stp-ms-lessons/src/test/java/com/sytoss/lessons/services/GroupServiceTest.java
+++ stp-ms-lessons/src/test/java/com/sytoss/lessons/services/GroupServiceTest.java
@@ -1,5 +1,6 @@
 package com.sytoss.lessons.services;
 
+import com.sytoss.domain.bom.users.Group;
 import com.sytoss.domain.bom.users.Teacher;
 import com.sytoss.lessons.connectors.GroupReferenceConnector;
 import com.sytoss.lessons.dto.DisciplineDTO;
@@ -43,7 +44,7 @@ class GroupServiceTest extends StpUnitTest {
         List<GroupReferenceDTO> input = new ArrayList<>();
         input.add(group);
         when(groupConnector.findByDisciplineId_TeacherId(1L)).thenReturn(input);
-        List<GroupReferenceDTO> groupReferenceDTOList = groupService.findGroups();
-        Assertions.assertEquals(1, groupReferenceDTOList.size());
+        List<Group> groups = groupService.findGroups();
+        Assertions.assertEquals(1, groups.size());
     }
 }
