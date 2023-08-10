f712e57af5e8fbfd5b39eaa5079f020343cc4514
Vasylyev Yevgeniy <yevgeny.vasilyev@extern.a1.at>
2023-08-10 17:56:37 +0300

    [STP-xxx] Introduce variable for image provider

--- stp-ms-test-producer/src/main/java/com/sytoss/producer/connectors/ImageConnector.java
+++ stp-ms-test-producer/src/main/java/com/sytoss/producer/connectors/ImageConnector.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(url = "http://localhost:9104/api", name = "ImageConnector")
+@FeignClient(url = "${image-provider-url}", name = "ImageConnector")
 public interface ImageConnector {
     @PostMapping("/convert/image")
     Long convertImage(@RequestBody String question);
