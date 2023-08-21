87c5a36410034027022101c7a5b5c2e91cb43352
Rasvel <ivan.larin@sytoss.com>
2023-08-21 18:00:05 +0300

    [STP-490] - Changed grade

--- stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/service/ScoreService.java
+++ stp-ms-check-task/src/main/java/com/sytoss/checktask/stp/service/ScoreService.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -66,17 +67,16 @@ public class ScoreService {
     }
 
     private boolean checkQueryResults(QueryResult queryResultEtalon, QueryResult queryResultAnswer) {
-        if (queryResultEtalon.getResultMapList().size() != queryResultAnswer.getResultMapList().size() && queryResultEtalon.getRow(0).size() != queryResultAnswer.getRow(0).size()) {
+        if (queryResultEtalon.getResultMapList().size() != queryResultAnswer.getResultMapList().size()) {
             return false;
         }
         for (int i = 0; i < queryResultEtalon.getResultMapList().size(); i++) {
             List<String> keyListEtalon = queryResultEtalon.getResultMapList().get(i).keySet().stream().toList();
             List<String> keyListAnswer = queryResultAnswer.getResultMapList().get(i).keySet().stream().toList();
-            if (keyListAnswer.size() != keyListEtalon.size()) {
+            if (keyListAnswer.size() < keyListEtalon.size()) {
                 return false;
             }
-            boolean allColumnsExists = keyListEtalon.stream()
-                    .allMatch(keyListAnswer::contains);
+            boolean allColumnsExists = new HashSet<>(keyListAnswer).containsAll(keyListEtalon);
 
             if (!allColumnsExists) {
                 return false;
@@ -84,6 +84,8 @@ public class ScoreService {
             for (String columnName : keyListEtalon) {
                 Object etalonFieldValue = queryResultEtalon.getResultMapList().get(i).get(columnName);
                 Object answerFieldValue = queryResultAnswer.getResultMapList().get(i).get(columnName);
+                etalonFieldValue = etalonFieldValue!=null ? etalonFieldValue : "null";
+                answerFieldValue = answerFieldValue!=null ? answerFieldValue : "null";
                 if (!etalonFieldValue.equals(answerFieldValue)) {
                     return false;
                 }
