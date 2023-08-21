4a950e3d13e2a539a20587c3c250d876c99a8e2d
anastasia_kharchenko <anastasia.kharchenko@sytoss.com>
2023-08-21 13:52:51 +0300

    [STP-482] remove unused code

--- stp-ms-test-producer/src/main/java/com/sytoss/producer/services/PersonalExamService.java
+++ stp-ms-test-producer/src/main/java/com/sytoss/producer/services/PersonalExamService.java
@@ -18,7 +18,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.*;
 
