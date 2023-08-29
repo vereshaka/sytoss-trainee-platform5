9b70da0dfc9d42d2dcbfd4b153d7d870527355da
YehorBodenchuk <120697528+YehorBodenchuk@users.noreply.github.com>
2023-08-29 12:53:41 +0300

    [STP-565] Added tests for endpoints and services

--- stp-ms-lessons/src/test/java/com/sytoss/lessons/controllers/TaskControllerTest.java
+++ stp-ms-lessons/src/test/java/com/sytoss/lessons/controllers/TaskControllerTest.java
@@ -3,7 +3,9 @@ package com.sytoss.lessons.controllers;
 import com.sytoss.domain.bom.exceptions.business.TaskExistException;
 import com.sytoss.domain.bom.exceptions.business.notfound.TaskNotFoundException;
 import com.sytoss.domain.bom.checktask.QueryResult;
+import com.sytoss.domain.bom.lessons.Exam;
 import com.sytoss.domain.bom.lessons.Task;
+import com.sytoss.domain.bom.lessons.Topic;
 import com.sytoss.lessons.bom.TaskDomainRequestParameters;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.ParameterizedTypeReference;
@@ -87,5 +89,26 @@ public class TaskControllerTest extends LessonsControllerTest {
         HttpEntity<TaskDomainRequestParameters> requestEntity = new HttpEntity<>(new TaskDomainRequestParameters(), httpHeaders);
         ResponseEntity<QueryResult> result = doPost("/api/task/check-request-result", requestEntity, QueryResult.class);
         assertEquals(200, result.getStatusCode().value());
+    }
+
+    @Test
+    public void shouldReturnTopicsByTaskId() {
+        when(taskService.getTopics(1L)).thenReturn(List.of(new Topic()));
+        HttpHeaders httpHeaders = getDefaultHttpHeaders();
+        HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<List<Topic>> result = doGet("/api/task/1/topics", httpEntity, new ParameterizedTypeReference<>() {
+        });
+        assertEquals(200, result.getStatusCode().value());
+    }
+
+    @Test
+    public void shouldReturnExamsByTaskId() {
+        when(taskService.getExams(1L)).thenReturn(List.of(new Exam()));
+        HttpHeaders httpHeaders = getDefaultHttpHeaders();
+        HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<List<Exam>> result = doGet("/api/task/1/exams", httpEntity, new ParameterizedTypeReference<>() {
+        });
+        assertEquals(200, result.getStatusCode().value());
+
     }
 }
