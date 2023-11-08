5e56c62427bcd28662f64eeee62545cb99894d26
Rasvel <ivan.larin@sytoss.com>
2023-11-08 19:39:42 +0200

    [STP-752-1] - Changed request body

---
+++ stp-ms-test-producer/src/main/java/com/sytoss/producer/bom/AnswersModel.java
@@ -0,0 +1,11 @@
+package com.sytoss.producer.bom;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnswersModel {
+    private String taskAnswer;
+    private String checkAnswer;
+}
