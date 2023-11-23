58c25d3fe9a9859f975a203a555e24ceac4bb711
Vasylyev Yevgeniy <yevgeny.vasilyev@extern.a1.at>
2023-11-23 12:51:19 +0200

    [CCF-xxx] Bugfix

--- stp-ms-lessons/src/main/java/com/sytoss/lessons/services/AnalyticsService.java
+++ stp-ms-lessons/src/main/java/com/sytoss/lessons/services/AnalyticsService.java
@@ -100,7 +100,7 @@ public class AnalyticsService extends AbstractService {
             analytic.getExam().setId(examConnector.findByExamAssignees_Id(personalExam.getExamAssigneeId()).getId());
             analytic.setStudent(personalExam.getStudent());
             analytic.setPersonalExam(personalExam);
-            analytic.setGrade(new AnalyticGrade(personalExam.getSummaryGrade(), personalExam.getSpentTime()));
+            analytic.setGrade(new AnalyticGrade(personalExam.getSummaryGrade(), personalExam.getSpentTime() == null ? 0 : personalExam.getSpentTime()));
             analytic.setStartDate(personalExam.getStartedDate());
             updateAnalytic(analytic);
             analytics.add(analytic);
