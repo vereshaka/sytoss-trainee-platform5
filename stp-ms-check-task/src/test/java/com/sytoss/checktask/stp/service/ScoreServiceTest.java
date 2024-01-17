1a811b44905f57ef5b8cc1db34da21cf657bf547
ivanl <ivan.larin@sytoss.com>
2024-01-17 22:21:13 +0200

    [STP-919] - fixed tests

--- stp-ms-check-task/src/test/java/com/sytoss/checktask/stp/service/ScoreServiceTest.java
+++ stp-ms-check-task/src/test/java/com/sytoss/checktask/stp/service/ScoreServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.ObjectProvider;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -51,6 +52,32 @@ class ScoreServiceTest extends StpUnitTest {
 
     @Test
     void checkAndGradeCorrectAnswerWithDifferentColumnsOrder() {
+        QueryResult answerResult = new QueryResult();
+        answerResult.setHeader(List.of("ID", "NAME"));
+        HashMap<String, Object> map11 = new LinkedHashMap<>();
+        map11.put("ID", 1L);
+        map11.put("NAME", "Ivan Ivanov");
+        answerResult.addValues(map11);
+        HashMap<String, Object> map12 = new LinkedHashMap<>();
+        map12.put("ID", 2L);
+        map12.put("NAME", "Ivan Petrov");
+        answerResult.addValues(map12);
+
+        QueryResult etalonResult = new QueryResult();
+        etalonResult.setHeader(List.of("NAME", "ID"));
+        HashMap<String, Object> map21 = new LinkedHashMap<>();
+        map21.put("NAME", "Ivan Ivanov");
+        map21.put("ID", 1L);
+        etalonResult.addValues(map21);
+        HashMap<String, Object> map22 = new LinkedHashMap<>();
+        map22.put("NAME", "Ivan Petrov");
+        map22.put("ID", 2L);
+        etalonResult.addValues(map22);
+
+        when(executorService.submit(any(Callable.class)))
+                .thenReturn(getFutureFor(answerResult))
+                .thenReturn(getFutureFor(etalonResult));
+
         CheckTaskParameters checkTaskParameters = new CheckTaskParameters();
         checkTaskParameters.setRequest("select id,name from Authors");
         checkTaskParameters.setEtalon("select name,id from Authors");
@@ -64,6 +91,45 @@ class ScoreServiceTest extends StpUnitTest {
 
     @Test
     void checkAndGradeWrongAnswer() {
+        QueryResult answerResult = new QueryResult();
+        answerResult.setHeader(List.of("ID", "BOOK_NAME", "AUTHOR_ID"));
+        HashMap<String, Object> map11 = new LinkedHashMap<>();
+        map11.put("ID", 1L);
+        map11.put("BOOK_NAME", "Book 1");
+        map11.put("AUTHOR_ID", 1);
+        answerResult.addValues(map11);
+        HashMap<String, Object> map12 = new LinkedHashMap<>();
+        map12.put("ID", 2L);
+        map12.put("BOOK_NAME", "Book 2");
+        map12.put("AUTHOR_ID", 1);
+        answerResult.addValues(map12);
+        HashMap<String, Object> map13 = new LinkedHashMap<>();
+        map13.put("ID", 3L);
+        map13.put("BOOK_NAME", "Book 3");
+        map13.put("AUTHOR_ID", 1);
+        answerResult.addValues(map13);
+        HashMap<String, Object> map14 = new LinkedHashMap<>();
+        map14.put("ID", 4L);
+        map14.put("BOOK_NAME", "Book 4");
+        map14.put("AUTHOR_ID", 2);
+        answerResult.addValues(map14);
+        HashMap<String, Object> map15 = new LinkedHashMap<>();
+        map15.put("ID", 5L);
+        map15.put("BOOK_NAME", "Book 5");
+        map15.put("AUTHOR_ID", 2);
+        answerResult.addValues(map15);
+
+        QueryResult etalonResult = new QueryResult();
+        etalonResult.setHeader(List.of("NAME", "ID"));
+        HashMap<String, Object> map21 = new LinkedHashMap<>();
+        map21.put("NAME", "Ivan Ivanov");
+        map21.put("ID", 1L);
+        etalonResult.addValues(map21);
+        HashMap<String, Object> map22 = new LinkedHashMap<>();
+        map22.put("NAME", "Ivan Petrov");
+        map22.put("ID", 2L);
+        etalonResult.addValues(map22);
+
         when(objectProvider.getObject()).thenReturn(
                 new DatabaseHelperService(new QueryResultConvertor(), new H2Executor()),
                 new DatabaseHelperService(new QueryResultConvertor(), new H2Executor())
@@ -83,6 +149,33 @@ class ScoreServiceTest extends StpUnitTest {
 
     @Test
     public void checkAndGradeWithoutConditions() {
+        QueryResult answerResult = new QueryResult();
+        answerResult.setHeader(List.of("ID", "NAME"));
+        HashMap<String, Object> map12 = new LinkedHashMap<>();
+        map12.put("ID", 2L);
+        map12.put("NAME", "Ivan Petrov");
+        answerResult.addValues(map12);
+        HashMap<String, Object> map11 = new LinkedHashMap<>();
+        map11.put("ID", 1L);
+        map11.put("NAME", "Ivan Ivanov");
+        answerResult.addValues(map11);
+
+        QueryResult etalonResult = new QueryResult();
+        etalonResult.setHeader(List.of("NAME", "ID"));
+        HashMap<String, Object> map21 = new LinkedHashMap<>();
+        map21.put("NAME", "Ivan Ivanov");
+        map21.put("ID", 1L);
+        etalonResult.addValues(map21);
+        HashMap<String, Object> map22 = new LinkedHashMap<>();
+        map22.put("NAME", "Ivan Petrov");
+        map22.put("ID", 2L);
+        etalonResult.addValues(map22);
+
+        when(executorService.submit(any(Callable.class)))
+                .thenReturn(getFutureFor(answerResult))
+                .thenReturn(getFutureFor(etalonResult));
+
+
         CheckTaskParameters checkTaskParameters = new CheckTaskParameters();
         checkTaskParameters.setRequest("select * from Authors");
         checkTaskParameters.setEtalon("select * from Authors ORDER BY Name");
@@ -100,6 +193,61 @@ class ScoreServiceTest extends StpUnitTest {
 
     @Test
     void checkAndGradeCorrectAnswerWithDifferentColumnsNumber() {
+        QueryResult answerResult = new QueryResult();
+        answerResult.setHeader(List.of("ID", "BOOK_NAME", "AUTHOR_ID"));
+        HashMap<String, Object> map11 = new LinkedHashMap<>();
+        map11.put("ID", 1L);
+        map11.put("BOOK_NAME", "Book 1");
+        map11.put("AUTHOR_ID", 1);
+        answerResult.addValues(map11);
+        HashMap<String, Object> map12 = new LinkedHashMap<>();
+        map12.put("ID", 2L);
+        map12.put("BOOK_NAME", "Book 2");
+        map12.put("AUTHOR_ID", 1);
+        answerResult.addValues(map12);
+        HashMap<String, Object> map13 = new LinkedHashMap<>();
+        map13.put("ID", 3L);
+        map13.put("BOOK_NAME", "Book 3");
+        map13.put("AUTHOR_ID", 1);
+        answerResult.addValues(map13);
+        HashMap<String, Object> map14 = new LinkedHashMap<>();
+        map14.put("ID", 4L);
+        map14.put("BOOK_NAME", "Book 4");
+        map14.put("AUTHOR_ID", 2);
+        answerResult.addValues(map14);
+        HashMap<String, Object> map15 = new LinkedHashMap<>();
+        map15.put("ID", 5L);
+        map15.put("BOOK_NAME", "Book 5");
+        map15.put("AUTHOR_ID", 2);
+        answerResult.addValues(map15);
+
+        QueryResult etalonResult = new QueryResult();
+        etalonResult.setHeader(List.of("ID", "BOOK_NAME"));
+        HashMap<String, Object> map21 = new LinkedHashMap<>();
+        map21.put("ID", 1L);
+        map21.put("BOOK_NAME", "Book 1");
+        etalonResult.addValues(map21);
+        HashMap<String, Object> map22 = new LinkedHashMap<>();
+        map22.put("ID", 2L);
+        map22.put("BOOK_NAME", "Book 2");
+        etalonResult.addValues(map22);
+        HashMap<String, Object> map23 = new LinkedHashMap<>();
+        map23.put("ID", 3L);
+        map23.put("BOOK_NAME", "Book 3");
+        etalonResult.addValues(map23);
+        HashMap<String, Object> map24 = new LinkedHashMap<>();
+        map24.put("ID", 4L);
+        map24.put("BOOK_NAME", "Book 4");
+        etalonResult.addValues(map24);
+        HashMap<String, Object> map25 = new LinkedHashMap<>();
+        map25.put("ID", 5L);
+        map25.put("BOOK_NAME", "Book 5");
+        etalonResult.addValues(map25);
+
+        when(executorService.submit(any(Callable.class)))
+                .thenReturn(getFutureFor(answerResult))
+                .thenReturn(getFutureFor(etalonResult));
+
         CheckTaskParameters checkTaskParameters = new CheckTaskParameters();
         checkTaskParameters.setRequest("select id,book_name,author_id from Books");
         checkTaskParameters.setEtalon("select book_name,id from Books");
@@ -113,6 +261,22 @@ class ScoreServiceTest extends StpUnitTest {
 
     @Test
     void checkAndGradeWrongData() {
+        QueryResult answerResult = new QueryResult();
+        answerResult.setHeader(List.of("COUNT"));
+        HashMap<String, Object> map11 = new LinkedHashMap<>();
+        map11.put("COUNT", 2);
+        answerResult.addValues(map11);
+
+        QueryResult etalonResult = new QueryResult();
+        etalonResult.setHeader(List.of("SUM"));
+        HashMap<String, Object> map21 = new LinkedHashMap<>();
+        map21.put("SUM", 3);
+        etalonResult.addValues(map21);
+
+        when(executorService.submit(any(Callable.class)))
+                .thenReturn(getFutureFor(answerResult))
+                .thenReturn(getFutureFor(etalonResult));
+
         CheckTaskParameters checkTaskParameters = new CheckTaskParameters();
         checkTaskParameters.setRequest("select count(*) from Authors");
         checkTaskParameters.setEtalon("select sum(id) from Authors");
@@ -163,6 +327,29 @@ class ScoreServiceTest extends StpUnitTest {
 
     @Test
     void checkAndGradeEtalonColumnsNotFound() {
+        QueryResult answerResult = new QueryResult();
+        answerResult.setHeader(List.of("NAME"));
+        HashMap<String, Object> map11 = new LinkedHashMap<>();
+        map11.put("NAME", "Ivan Ivanov");
+        answerResult.addValues(map11);
+        HashMap<String, Object> map12 = new LinkedHashMap<>();
+        map12.put("NAME", "Ivan Petrov");
+        answerResult.addValues(map12);
+
+        QueryResult etalonResult = new QueryResult();
+        etalonResult.setHeader(List.of("NAME", "ID"));
+        HashMap<String, Object> map21 = new LinkedHashMap<>();
+        map21.put("NAME", "Ivan Ivanov");
+        map21.put("ID", 1L);
+        etalonResult.addValues(map21);
+        HashMap<String, Object> map22 = new LinkedHashMap<>();
+        map22.put("NAME", "Ivan Petrov");
+        map22.put("ID", 2L);
+        etalonResult.addValues(map22);
+
+        when(executorService.submit(any(Callable.class)))
+                .thenReturn(getFutureFor(answerResult))
+                .thenReturn(getFutureFor(etalonResult));
 
         CheckTaskParameters checkTaskParameters = new CheckTaskParameters();
         checkTaskParameters.setRequest("select name from Authors");
@@ -181,6 +368,11 @@ class ScoreServiceTest extends StpUnitTest {
         checkRequestParameters.setRequest("insert into Authors(id,name) values (100,'Author 2')");
         checkRequestParameters.setCheckAnswer("select * from Authors");
         checkRequestParameters.setScript(readFromFile("task-domain/script1.json"));
+
+        when(objectProvider.getObject()).thenReturn(
+                new DatabaseHelperService(new QueryResultConvertor(), new H2Executor()),
+                new DatabaseHelperService(new QueryResultConvertor(), new H2Executor())
+        );
 
         scoreService.checkRequest(checkRequestParameters);
     }
